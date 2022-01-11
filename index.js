111b37e5b39e56545ebb76ce1a33141b7953f455
divertome <hans9645@gmail.com>
2022-01-11 20:34:51 +0900

    0110

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose
-  .connect(
-    "mongodb+srv://hans9645:1234@boilerplate.fg2ff.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      //   useNewUrlParser: true,
-      //   useUnifiedTopology: true,
-      //   useCreateIndex: true,
-      //   useFindAndModify: false,
-      //mongoose 6.0이상부터 자동으로 위 설정이 반영된다. 따라서 필요없어짐.
-    }
-  )
+  .connect(process.env.MongoURL, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: false,
+    //mongoose 6.0이상부터 자동으로 위 설정이 반영된다. 따라서 필요없어짐.
+  })
   .then(() => {
     console.log("MongoDB connected...");
   })

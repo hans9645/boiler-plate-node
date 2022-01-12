00b7e593c3fec0078b314f76d503da1ddc8ab6b6
divertome <hans9645@gmail.com>
2022-01-12 17:52:05 +0900

    mongodb setting and register API

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+
+//application/x-www-form-lencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
+
 require("dotenv").config();
 
 mongoose
-  .connect(process.env.MongoURL, {
+  .connect(config.mongoURI, {
     //   useNewUrlParser: true,
     //   useUnifiedTopology: true,
     //   useCreateIndex: true,
@@ -21,6 +30,15 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
+});
+
+app.post("/register", (req, res) => {
+  //회원가입 시 필요한 정보를 client로 부터 받아씀.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

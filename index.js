ab2a1347f1e7de05641473c94dfac02a155b5f5a
divertome <hans9645@gmail.com>
2022-01-12 22:12:39 +0900

    can't understand arrow function default parameter err

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const { User } = require("./models/User");
 const bodyParser = require("body-parser");
 const config = require("./config/key");
+const { json } = require("body-parser");
 
 //application/x-www-form-lencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -35,9 +36,35 @@ app.get("/", (req, res) => {
 app.post("/register", (req, res) => {
   //회원가입 시 필요한 정보를 client로 부터 받아씀.
   const user = new User(req.body);
+
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
+  });
+});
+
+app.post("login", (req, res) => {
+  //요청된 이메일이 데이터베이스에 있는 지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+
+    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 같은 지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      }
+    });
+
+    //비밀번호까지 같다면 토큰 생성.
+    user.generateToken((err, user) => {});
   });
 });
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -10,7 +12,7 @@ const userSchema = mongoose.Schema({
     trim: true, //공백을 알아서 제거해주는 역할
     uniqe: 1,
   },
-  passwod: {
+  password: {
     type: String,
     minlength: 5,
   },
@@ -30,6 +32,32 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre("save", function (next) {
+  var user = this;
+  //  비밀번호를 암호화 시킴.
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        // Store hash in your password DB.
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    return cb(null, isMatch);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 //schema는 모델로 감싸줘야한다.
